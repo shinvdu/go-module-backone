60f9b9a9d5bc1b60c44ba403b87566d033e5a066
tianbao <silas@sky-city.me>
2022-08-04 18:27:52 +0800

    script to replace project name

--- new_project.sh
+++ new_project.sh
@@ -2,15 +2,15 @@
 
 set -o errexit
 
-# DOMAIN=git2.sky-city.me  USERNAME=silas PROJECT_NAME=govpn ./new_project.sh
+# DOMAIN=github.com  AUSER=silas PROJECT_NAME=govpn ./new_project.sh
 
 if [[ -z $DOMAIN ]]; then
 	# echo 'please input your username';
 	read -p "Enter your domain(no space): " DOMAIN
 fi
 
-if [[ -z $USER_NAME ]]; then
-	read -p "Enter your username(no space): " USER_NAME
+if [[ -z $AUSER ]]; then
+	read -p "Enter your username(no space): " AUSER
 fi
 
 
@@ -19,37 +19,54 @@ if [[ -z $PROJECT_NAME ]]; then
 fi
 
 echo "domain: $DOMAIN"
-echo "username: $USER_NAME"
+echo "username: $AUSER"
 echo "project: $PROJECT_NAME"
 
-# mv src/git.sky-city.me "src/${DOMAIN}"
-# mv "src/${DOMAIN}/tony" "src/${DOMAIN}/${USERNAME}"
-# mv "src/${DOMAIN}/${USERNAME}/project" "src/${DOMAIN}/${USERNAME}/${PROJECT_NAME}"
+export DOMAIN
+export AUSER
+export PROJECT_NAME
 
-# mv  "src/${DOMAIN}/${USERNAME}/${PROJECT_NAME}/project.go" "src/${DOMAIN}/${USERNAME}/${PROJECT_NAME}/${PROJECT_NAME}.go"
+
 if [[ ! -d  src/git.sky-city.me/tony/project/ ]]; then
 	echo 'exit, you have generate the project!'
 	exit 1;
 fi
 
-cp -r src/git.sky-city.me/tony/project/ "$PROJECT_NAME"
+export PROJECT_PATH="src/${DOMAIN}/${AUSER}/${PROJECT_NAME}"
 
-rm -fR src/git.sky-city.me
+function filepathChange {
 
-mkdir -p "src/${DOMAIN}/${USERNAME}/${PROJECT_NAME}/cmd"
-mkdir -p "src/${DOMAIN}/${USERNAME}/${PROJECT_NAME}/cmd/${PROJECT_NAME}-client/"
-mkdir -p "src/${DOMAIN}/${USERNAME}/${PROJECT_NAME}/cmd/${PROJECT_NAME}-server/"
-mkdir -p "src/${DOMAIN}/${USERNAME}/${PROJECT_NAME}/config"
+	cp -r src/git.sky-city.me/tony/project/ "$PROJECT_NAME"
 
-cp "${PROJECT_NAME}/cmd/project-client/project-client.go" "src/${DOMAIN}/${USERNAME}/${PROJECT_NAME}/cmd/${PROJECT_NAME}-client/${PROJECT_NAME}-client.go"
+	rm -fR src/git.sky-city.me
 
-cp "${PROJECT_NAME}/cmd/project-server/project-server.go" "src/${DOMAIN}/${USERNAME}/${PROJECT_NAME}/cmd/${PROJECT_NAME}-server/${PROJECT_NAME}-server.go"
+	mkdir -p "src/${DOMAIN}/${AUSER}/${PROJECT_NAME}/cmd"
+	mkdir -p "src/${DOMAIN}/${AUSER}/${PROJECT_NAME}/cmd/${PROJECT_NAME}-client/"
+	mkdir -p "src/${DOMAIN}/${AUSER}/${PROJECT_NAME}/cmd/${PROJECT_NAME}-server/"
+	mkdir -p "src/${DOMAIN}/${AUSER}/${PROJECT_NAME}/config"
 
-cp "${PROJECT_NAME}/project.go" "src/${DOMAIN}/${USERNAME}/${PROJECT_NAME}/${PROJECT_NAME}.go"
+	cp "${PROJECT_NAME}/cmd/project-client/project-client.go" "src/${DOMAIN}/${AUSER}/${PROJECT_NAME}/cmd/${PROJECT_NAME}-client/${PROJECT_NAME}-client.go"
 
-cp "${PROJECT_NAME}/config/config.go" "src/${DOMAIN}/${USERNAME}/${PROJECT_NAME}/config/config.go"
+	cp "${PROJECT_NAME}/cmd/project-server/project-server.go" "src/${DOMAIN}/${AUSER}/${PROJECT_NAME}/cmd/${PROJECT_NAME}-server/${PROJECT_NAME}-server.go"
 
-rm -fR "$PROJECT_NAME"
+	cp "${PROJECT_NAME}/project.go" "src/${DOMAIN}/${AUSER}/${PROJECT_NAME}/${PROJECT_NAME}.go"
+
+	cp "${PROJECT_NAME}/config/config.go" "src/${DOMAIN}/${AUSER}/${PROJECT_NAME}/config/config.go"	
+	rm -fR "$PROJECT_NAME"
+
+}
+
+function replaceToken {
+	perl -pi -e 's#git.sky-city.me\/tony\/project#$ENV{PROJECT_PATH}#g' "$1"
+	perl -pi -e 's#project#$ENV{PROJECT_NAME}#g' "$1"
+	perl -pi -e 's#tony#$ENV{AUSER}#g' "$1"
+	perl -pi -e 's#git.sky-city.me#$ENV{DOMAIN}#g' "$1"
+}
 
 
+filepathChange
+
+replaceToken "${PROJECT_PATH}/${PROJECT_NAME}.go"
+replaceToken "${PROJECT_PATH}/cmd/${PROJECT_NAME}-client/${PROJECT_NAME}-client.go"
+replaceToken "${PROJECT_PATH}/cmd/${PROJECT_NAME}-server/${PROJECT_NAME}-server.go"
 
